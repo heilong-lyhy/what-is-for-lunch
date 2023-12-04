a1549166ba4f91ff860bf443c6a78014479f64da
heilong_lyhy <mjlyhy@outlook.com>
2023-12-04 21:28:22 +0800

    修复部分bug

--- src/Clicktime/index.js
+++ src/Clicktime/index.js
@@ -1,52 +1,90 @@
-import { useRef, useState } from 'react';
+import React, { Component } from 'react';
 
-
-
-function Clicktime({cisclick,cfoudlist = []}){
-  const refOBJ = useRef()
-  if(cisclick){
-    for (let i = 0; i < 1; i++) { // 创建1个文本
-      const randomIndex = Math.floor(Math.random() * cfoudlist.length);
-      var randomText = cfoudlist[randomIndex];
-      // let textElement = document.createElement("div");
-      // textElement.textContent = randomText;
-      // textElement.className = "floating-text";
-
-
-      // 设置随机位置
-      const maxX = window.innerWidth;
-      const maxY = window.innerHeight;
-      const randomX = Math.random() * maxX;
-      const randomY = Math.random() * maxY;
-      refOBJ.current.style.left = randomX + "px";
-      refOBJ.current.style.top = randomY + "px";
-
-      // // 设置随机字体大小
-      const randomSize = Math.floor(Math.random() * 30) + 12;
-      refOBJ.current.style.fontSize = randomSize + "px";
-
-      // 添加文本到页面
-      
-
-      // 渐入效果
-      setTimeout(() => {
-        refOBJ.current.style.opacity = 1;
-      }, 1000);
-
-      // 自动移除文本
-      setTimeout(() => {
-        // 渐出效果
-        refOBJ.current.style.opacity = 0;
-         // 渐出效果持续2秒
-      }, 1000); // 文本显示2秒后移除
-    }
-
-    // 继续下一个文本的动画
-    setTimeout(Clicktime, 300);
+class FloatingTextEffect extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isAnimating: false,
+      textElements: [],
+      yourTexts: ["文本1", "文本2", "文本3", "文本4", "文本5"]
+    };
   }
-  return(
-    <div className='floating-text' ref={refOBJ}>{randomText}</div>
-  )
+
+  toggleAnimation = () => {
+    if (this.state.isAnimating) {
+      // 停止特效并清空文本元素数组
+      this.state.textElements.forEach(textElement => textElement.timer && clearTimeout(textElement.timer));
+      this.setState({
+        isAnimating: false,
+        textElements: []
+      });
+    } else {
+      // 开始特效
+      this.setState({ isAnimating: true });
+      this.animateText();
+    }
+  }
+
+  animateText = () => {
+    if (this.state.isAnimating) {
+      for (let i = 0; i < 3; i++) {
+        const randomIndex = Math.floor(Math.random() * this.state.yourTexts.length);
+        const randomText = this.state.yourTexts[randomIndex];
+
+        // 创建文本元素
+        const textElement = (
+          <div
+            key={Date.now() + i}
+            className="floating-text"
+            style={{
+              left: `${Math.random() * (window.innerWidth - 100)}px`,
+              top: `${Math.random() * (window.innerHeight - 50)}px`,
+              fontSize: `${Math.floor(Math.random() * 30) + 12}px`,
+              opacity: 0
+            }}
+          >
+            {randomText}
+          </div>
+        );
+
+        // 添加文本元素到数组
+        this.setState(prevState => ({
+          textElements: [...prevState.textElements, textElement]
+        }));
+
+        // 设置渐入效果
+        setTimeout(() => {
+          textElement.props.style.opacity = 1;
+          this.forceUpdate(); // 更新渲染以应用样式
+        }, 0);
+
+        // 设置渐出效果并移除文本元素
+        textElement.timer = setTimeout(() => {
+          textElement.props.style.opacity = 0;
+          this.forceUpdate(); // 更新渲染以应用样式
+          setTimeout(() => {
+            this.setState(prevState => ({
+              textElements: prevState.textElements.filter(elem => elem !== textElement)
+            }));
+          }, 1000); // 渐出效果持续1秒
+        }, 2000); // 文本显示2秒后移除
+      }
+
+      // 继续下一个文本的动画
+      setTimeout(this.animateText, 2000);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.toggleAnimation}>
+          {this.state.isAnimating ? "归队" : "开始特效"}
+        </button>
+        {this.state.textElements}
+      </div>
+    );
+  }
 }
 
-export default Clicktime;
+export default FloatingTextEffect;
--- src/Home/index.js
+++ src/Home/index.js
@@ -1,20 +1,23 @@
 import './index.css';
-import { useState } from 'react';
+import { useEffect,useState } from 'react';
 import Btn from '../Btn'
 // import React, { Component } from 'react';
 
 function Home(){
-  const [istime, setistime] = useState('什么');
-  const [isclick, setisclick] = useState(true);
+  const [istext, setistext] = useState('什么');
   const [foudlist, setfoudlist] = useState(['1','2','3'])
+  const [textElements, settextElements] = useState([])
+  const [clickkey, setclickkey] = useState(0);
+  const [isclick, setisclick] = useState(true);
   // let isclick = false;
   // let foudlist = ['1','2','3'];
   const body = document.body;
   let text = ''
+  let torf = false
   if(isclick){
-    text = '结束'
-  }else{
     text = '开始'
+  }else{
+    text = '结束'
   }
   
   
@@ -26,14 +29,12 @@ function Home(){
   // }, 1000);
   function click(){
     if(isclick){
-      setisclick(n => n=false)
-      // isclick = false;
-      // clear();
       clicktime();
+      setisclick(false)
+      torf = false
     }else{
-      setisclick(n => n=true)
-      // isclick = true;
-      
+      setisclick(true)
+      torf = true
     }
   }
 
@@ -43,7 +44,7 @@ function Home(){
       for (let i = 0; i < 1; i++) { // 创建1个文本
         const randomIndex = Math.floor(Math.random() * foudlist.length);
         const randomText = foudlist[randomIndex];
-        setistime(randomText)
+        setistext(randomText)
         let textElement = document.createElement("div");
         textElement.textContent = randomText;
         textElement.className = "floating-text";
@@ -84,6 +85,48 @@ function Home(){
   }
 
 
+  // function clicktime(clickis) {
+  //   if(clickis === false){
+  //     for (let i = 0; i < 1; i++) { // 创建1个文本
+  //       setclickkey(n => n+1)
+  //       const randomIndex = Math.floor(Math.random() * foudlist.length);
+  //       const randomText = foudlist[randomIndex];
+  //       setistext(randomText)
+  //       const textElement = (
+  //         <div className="floating-text" key={clickkey} 
+  //         style={{position:"absolute",
+  //         left:`${Math.random() * (window.innerWidth - 100)}px`,
+  //         top:`${Math.random() * (window.innerHeight - 50)}px`,
+  //         fontSize: `${Math.floor(Math.random() * 30) + 12}px`,
+  //         opacity: 0,
+  //         transition: 'opacity 2s',
+  //         }}>{istext}</div>
+  //       )
+  //       // 添加文本到页面
+  //       settextElements(textElements.push(textElement));
+
+  //       // 渐入效果
+  //       setTimeout(() => {
+  //         textElement.props.style.opacity = 1;
+  //         // this.forceUpdate();
+  //       }, 1000);
+
+  //       // 自动移除文本
+  //       setTimeout(() => {
+  //         // 渐出效果
+  //         textElement.props.style.opacity = 0;
+  //         setTimeout(() => {
+  //           settextElements(textElements.filter(elem => elem !== textElements))
+  //           // this.forceUpdate();
+  //         }, 1000); // 渐出效果持续2秒
+  //       }, 2000); // 文本显示2秒后移除
+  //     }
+
+  //     // 继续下一个文本的动画
+  //     setTimeout(clicktime, 300);
+  //   }
+  // }
+
   //以下内容为实现列表自定义的函数
   function pushlist(){
     let newlist1 = foudlist
@@ -118,7 +161,7 @@ function Home(){
   return(
     <>
       <div className="main">
-        <span className='s-title'>今天中午吃<h3>{istime}</h3></span>
+        <span className='s-title'>今天中午吃<h3>{istext}</h3></span>
         <Btn ic={isclick} click={click} text={text}/>
         <Btn text={'添加列表项'} click={pushlist}/>
         <Btn text={'删除列表项'} click={poplist}/>
